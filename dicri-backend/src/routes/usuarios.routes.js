329a689193723b0c3ce32ff1e95b4d553585ab1e
Josue Martinez <dgudielo@miumg.edu.gt>
2025-11-23 00:47:06 -0600

    correcciones en el routes de usuarios

--- dicri-backend/src/routes/usuarios.routes.js
+++ dicri-backend/src/routes/usuarios.routes.js
@@ -4,12 +4,10 @@ const ctrl = require('../controllers/usuarios.controller');
 
 const router = Router();
 
-// Lecturas: cualquier autenticado
 router.get('/', authMiddleware, ctrl.list);
 router.get('/:id', authMiddleware, ctrl.getById);
 router.get('/:id/roles', authMiddleware, ctrl.getRoles);
 
-// Mutaciones: solo coordinador
 router.post('/', authMiddleware, authorize({ roles: ['COORDINADOR'] }), ctrl.create);
 router.put('/:id', authMiddleware, authorize({ roles: ['COORDINADOR'] }), ctrl.update);
 router.put('/:id/password', authMiddleware, authorize({ roles: ['COORDINADOR'] }), ctrl.updatePassword);
